773d145f34bb4ab955cd396dfc7e3c25e5f6b667
wuhui <wuhui@yadingdata.com>
2020-10-16 16:43:27 +0800

    初始化和setup和ref

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<img alt="Vue logo" src="../assets/logo.png">
+<div>
+  <h2>欢迎光临红浪漫洗浴中心</h2>
+  <div>请选择一位美女为你服务</div>
+</div>
+<div>
+  <button v-for="(item, index) in girls" v-bind:key="index" @click="selectGirlFun(index)">{{ index }} : {{ item }} </button>
+  <div>你选择了【{{ selectGirl }}】为你服务</div>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {
+  defineComponent,
+  ref
+} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  setup() {
+    const girls = ref(["大脚", "刘英", "晓红"]);
+    const selectGirl = ref("");
+    console.log(selectGirl)
+    const selectGirlFun = (index: number) => {
+      selectGirl.value = girls.value[index];
+    };
+    return {
+      girls,
+      selectGirl,
+      selectGirlFun
+    }
+  }
 });
 </script>
