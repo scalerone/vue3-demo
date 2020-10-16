11533543cddd467c0f37fdce52c3ae1a92569330
wuhui <wuhui@yadingdata.com>
2020-10-16 16:55:43 +0800

    Composition  API reactive改造程序

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,31 +5,37 @@
   <div>请选择一位美女为你服务</div>
 </div>
 <div>
-  <button v-for="(item, index) in girls" v-bind:key="index" @click="selectGirlFun(index)">{{ index }} : {{ item }} </button>
-  <div>你选择了【{{ selectGirl }}】为你服务</div>
+  <button v-for="(item, index) in data.girls" v-bind:key="index" @click="data.selectGirlFun(index)">{{ index }} : {{ item }} </button>
+  <div>你选择了【{{ data.selectGirl }}】为你服务</div>
 </div>
 </template>
 
 <script lang="ts">
 import {
   defineComponent,
+  reactive,
   ref
 } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
+interface DataProps {
+  girls: string[];
+  selectGirl: string;
+  selectGirlFun: (index: number) => void;
+}
 export default defineComponent({
   name: 'Home',
   setup() {
-    const girls = ref(["大脚", "刘英", "晓红"]);
-    const selectGirl = ref("");
-    console.log(selectGirl)
-    const selectGirlFun = (index: number) => {
-      selectGirl.value = girls.value[index];
-    };
+    const data: DataProps = reactive({
+      girls: ["大脚", "刘英", "晓红"],
+      selectGirl: "",
+      selectGirlFun: (index: number) => {
+        data.selectGirl = data.girls[index];
+      },
+    });
+    //http://www.liulongbin.top:8085/#/?id=_4-reactive
     return {
-      girls,
-      selectGirl,
-      selectGirlFun
+      data,
     }
   }
 });
