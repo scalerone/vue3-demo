6c280c47ccfab50d7ac52007eac0025d0591088e
wuhui <wuhui@yadingdata.com>
2020-10-19 11:27:24 +0800

    ✅ watch监听一个或多个值

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
-<img alt="Vue logo" src="../assets/logo.png">
+<img alt="Vue logo" src="../assets/logo.png" />
 <div>
   <h2>欢迎光临红浪漫洗浴中心</h2>
   <div>请选择一位美女为你服务</div>
 </div>
 <div>
-  <button v-for="(item, index) in girls" v-bind:key="index" @click="selectGirlFun(index)">{{ index }} : {{ item }} </button>
+  <button v-for="(item, index) in girls" v-bind:key="index" @click="selectGirlFun(index)">
+    {{ index }} : {{ item }}
+  </button>
   <div>你选择了【{{ selectGirl }}】为你服务</div>
 </div>
+<div>{{ overText }}</div>
+<div><button @click="overAction">点餐完毕</button></div>
 </template>
 
 <script lang="ts">
@@ -20,40 +24,44 @@ import {
   onBeforeMount,
   onBeforeUpdate,
   onUpdated,
-} from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  watch
+} from 'vue'
 
 interface DataProps {
   girls: string[];
   selectGirl: string;
+  count: number;
   selectGirlFun: (index: number) => void;
 }
 export default defineComponent({
   name: 'Home',
   setup() {
-    console.log("1-开始创建组件-----setup()");
+    console.log('1-开始创建组件-----setup()')
     const data: DataProps = reactive({
-      girls: ["大脚", "刘英", "晓红"],
-      selectGirl: "",
+      girls: ['大脚', '刘英', '晓红'],
+      selectGirl: '',
+      count: 0,
       selectGirlFun: (index: number) => {
-        data.selectGirl = data.girls[index];
+        data.selectGirl = data.girls[index]
+        data.count++;
+        console.log(data.count)
       },
-    });
+    })
     //http://www.liulongbin.top:8085/#/?id=_4-reactive
     onBeforeMount(() => {
-      console.log("2-组件挂载到页面之前执行-----onBeforeMount()");
-    });
+      console.log('2-组件挂载到页面之前执行-----onBeforeMount()')
+    })
 
     onMounted(() => {
-      console.log("3-组件挂载到页面之后执行-----onMounted()");
-    });
+      console.log('3-组件挂载到页面之后执行-----onMounted()')
+    })
     onBeforeUpdate(() => {
-      console.log("4-组件更新之前-----onBeforeUpdate()");
-    });
+      console.log('4-组件更新之前-----onBeforeUpdate()')
+    })
 
     onUpdated(() => {
-      console.log("5-组件更新之后-----onUpdated()");
-    });
+      console.log('5-组件更新之后-----onUpdated()')
+    })
 
     // Vue2-- -- -- -- -- -- --vue3
     // beforeCreate - > setup()
@@ -67,9 +75,33 @@ export default defineComponent({
     // activated - > onActivated
     // deactivated - > onDeactivated
     // errorCaptured - > onErrorCaptured
+
+    const overText = ref('honghong');
+    const overAction = () => {
+      overText.value = overText.value + '点餐完成 |'
+      // document.title = overText.value;
+      // data.count++;
+      // console.log(data.count)
+
+    };
+    //watch监听一个或多个值
+    // watch(overText, (newValue, oldValue) => {
+    //   console.log(`new--->${newValue}`);
+    //   console.log(`old--->${oldValue}`);
+    //   document.title = newValue;
+    // });
+    watch([overText, () => data.selectGirl, () => data.count], (newValue, oldValue) => {
+      console.log(`new--->${newValue}`);
+      console.log(`old--->${oldValue}`);
+      document.title = newValue[0] + '';
+    }, {
+      deep: true
+    });
     return {
       ...toRefs(data),
+      overText,
+      overAction
     }
-  }
-});
+  },
+})
 </script>
