3da7498d940dd15674318729477a84c9861a7a21
wuhui <wuhui@yadingdata.com>
2020-10-20 15:56:52 +0800

    😊返回事件方法和加loading

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
-  <input type="text" v-model="query" />
+  <input type="text" v-model="input" />
+  <button @click="setQuery">搜索</button>
+  <div v-if="loading">Loading ...</div>
+  <div v-else-if="error">Something went wrong ...</div>
   <ul>
     <li v-for="item of hits" :key="item.objectID">
       <a :href="item.url">{{item.title}}</a>
@@ -25,13 +28,24 @@ export default {
   setup() {
     const state = reactive({
       query: 'vue',
-      hits: []
+      input: 'vue',
+      hits: [],
+      error: false,
+      loading: false,
     })
     const fetchData = async (query: string) => {
-      const data = await fetch(
-        `https://hn.algolia.com/api/v1/search?query=${query}`
-      ).then(rsp => rsp.json())
-      state.hits = data.hits
+      state.error = false
+      state.loading = true
+      try {
+        const data = await fetch(
+          `https://hn.algolia.com/api/v1/search?query=${query}`
+        ).then(rsp => rsp.json())
+        state.hits = data.hits
+      } catch {
+        state.error = true
+      }
+      state.loading = false
+
     }
 
     onMounted(() => {
@@ -39,8 +53,13 @@ export default {
         fetchData(state.query)
       })
     })
+
+    const setQuery = () => {
+      state.query = state.input
+    }
     return {
       ...toRefs(state),
+      setQuery
     }
   }
 }
