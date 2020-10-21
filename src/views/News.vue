1ba81245b31a2f986db6b0c081120dff714cfb4c
wuhui <wuhui@yadingdata.com>
2020-10-21 11:34:15 +0800

    📦 将数据请求逻辑抽象到hooks

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,11 +1,11 @@
 <template>
 <div>
-  <input type="text" v-model="input" />
+  <input type="text" v-model="query" @keydown="setQuery" />
   <button @click="setQuery">搜索</button>
   <div v-if="loading">Loading ...</div>
   <div v-else-if="error">Something went wrong ...</div>
   <ul>
-    <li v-for="item of hits" :key="item.objectID">
+    <li v-for="item of data.hits" :key="item.objectID">
       <a :href="item.url">{{item.title}}</a>
     </li>
   </ul>
@@ -15,51 +15,39 @@
 
 <script lang="ts">
 import {
-  defineComponent,
   reactive,
   onMounted,
   toRefs,
-  watch,
-  watchEffect
+  ref
 } from 'vue'
+import useRequest from '../hooks/useRequest'
 
 export default {
   name: 'News',
   setup() {
-    const state = reactive({
-      query: 'vue',
-      input: 'vue',
-      hits: [],
-      error: false,
-      loading: false,
-    })
-    const fetchData = async (query: string) => {
-      state.error = false
-      state.loading = true
-      try {
-        const data = await fetch(
-          `https://hn.algolia.com/api/v1/search?query=${query}`
-        ).then(rsp => rsp.json())
-        state.hits = data.hits
-      } catch {
-        state.error = true
+    const query = ref('vue')
+    const {
+      data,
+      loading,
+      error,
+      run
+    } = useRequest({
+      url: 'https://hn.algolia.com/api/v1/search',
+      params: {
+        query
       }
-      state.loading = false
-
-    }
+    })
 
     onMounted(() => {
-      watchEffect(() => {
-        fetchData(state.query)
-      })
+      run()
     })
-
-    const setQuery = () => {
-      state.query = state.input
-    }
+    // console.log('data', data)
     return {
-      ...toRefs(state),
-      setQuery
+      data,
+      query,
+      error,
+      loading,
+      setQuery: run
     }
   }
 }
